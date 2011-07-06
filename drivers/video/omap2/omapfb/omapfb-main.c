9d998ff8b389f2a6d4d00c57438ccc7926a8dbde
Andy Doan <andy.doan@linaro.org>
2011-07-06 12:08:29 -0500

    OMAPFB: make debug message more useful
    Make the debug message useful by printing the name of the device
    that no associated driver could be found for.
    
    Signed-off-by: Andy Doan <andy.doan@linaro.org>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -2417,7 +2417,8 @@ static int omapfb_probe(struct platform_device *pdev)
 		omap_dss_get_device(dssdev);
 
 		if (!dssdev->driver) {
-			dev_err(&pdev->dev, "no driver for display\n");
+			dev_err(&pdev->dev, "no driver for display: %s\n",
+				dssdev->name);
 			r = -ENODEV;
 		}
 
