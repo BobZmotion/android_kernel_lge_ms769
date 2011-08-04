1af85cf2b2f1cfab5851c40437922f8806e7c6d4
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-08-04 14:37:29 +0300

    OMAP: OMAPFB: make omapfb start even when a display is missing a driver
    Currently omapfb wants that all the display devices have a driver,
    otherwise omapfb refuses to start. There's no real requirement to act
    like that, and this patch will make omapfb give a warning and skip that
    device.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -2417,9 +2417,10 @@ static int omapfb_probe(struct platform_device *pdev)
 		omap_dss_get_device(dssdev);
 
 		if (!dssdev->driver) {
-			dev_err(&pdev->dev, "no driver for display: %s\n",
+			dev_warn(&pdev->dev, "no driver for display: %s\n",
 				dssdev->name);
-			r = -ENODEV;
+			omap_dss_put_device(dssdev);
+			continue;
 		}
 
 		fbdev->displays[fbdev->num_displays++] = dssdev;
