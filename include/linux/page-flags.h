78e0ea646cbd72456f5950f4710ede2680708279
faux123 <reioux@gmail.com>
2012-02-07 21:33:32 -0800

    slub: Do not use frozen page flag but a bit in the page counters
    Do not use a page flag for the frozen bit. It needs to be part
    of the state that is handled with cmpxchg_double(). So use a bit
    in the counter struct in the page struct for that purpose.
    
    Signed-off-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>
    
    Conflicts:
    
    	include/linux/page-flags.h
    
    Conflicts:
    	include/linux/page-flags.h

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -125,9 +125,6 @@ enum pageflags {
 
 	/* SLOB */
 	PG_slob_free = PG_private,
-
-	/* SLUB */
-	PG_slub_frozen = PG_active,
 };
 
 #ifndef __GENERATING_BOUNDS_H
@@ -213,8 +210,9 @@ PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
 __PAGEFLAG(SlobFree, slob_free)
 
-__PAGEFLAG(SlubFrozen, slub_frozen)
-
+#ifdef CONFIG_CLEANCACHE
+PAGEFLAG(WasActive, was_active)
+#endif
 /*
  * Private page markings that may be used by the filesystem that owns the page
  * for its own purposes.
