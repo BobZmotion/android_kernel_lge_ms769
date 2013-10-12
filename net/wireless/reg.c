51bdf9cd03eb7fd5c063cf9027ab9e18d6ed1517
Mihir Shete <smihir@codeaurora.org>
2013-10-13 05:25:30 +0530

    cfg80211: add flags to define country IE processing rules
    802.11 cards may have different country IE parsing behavioural
    preferences and vendors may want to support these. These preferences
    were managed by the WIPHY_FLAG_CUSTOM_REGULATORY and the
    WIPHY_FLAG_STRICT_REGULATORY flags and their combination.
    Instead of using this existing notation, split out the country IE
    behavioural preferences to a new flag. This will allow us to add more
    customizations easily and make the code more maintainable. Also add
    a new flag to disable country IE hints issued by the CORE as the
    first customization.
    
    Change-Id: I66ba4a92ac0f029a115eea0a274b02db11279787
    CRs-Fixed: 542802
    Signed-off-by: Mihir Shete <smihir@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	include/net/cfg80211.h
    	net/wireless/reg.c

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -885,11 +885,20 @@ static void handle_channel(struct wiphy *wiphy,
 	chan->flags = flags | bw_flags | map_regdom_flags(reg_rule->flags);
 	chan->max_antenna_gain = min(chan->orig_mag,
 		(int) MBI_TO_DBI(power_rule->max_antenna_gain));
-	if (chan->orig_mpwr)
-		chan->max_power = min(chan->orig_mpwr,
-			(int) MBM_TO_DBM(power_rule->max_eirp));
-	else
-		chan->max_power = (int) MBM_TO_DBM(power_rule->max_eirp);
+	chan->max_power = (int) MBM_TO_DBM(power_rule->max_eirp);
+	if (chan->orig_mpwr) {
+		/*
+		 * Devices that use NL80211_COUNTRY_IE_FOLLOW_POWER will always
+		 * follow the passed country IE power settings.
+		 */
+		if (initiator == NL80211_REGDOM_SET_BY_COUNTRY_IE &&
+		    wiphy->country_ie_pref & NL80211_COUNTRY_IE_FOLLOW_POWER)
+			chan->max_power = chan->max_power;
+		else
+			chan->max_power = min(chan->orig_mpwr,
+					      chan->max_power);
+	} else
+		chan->max_power = chan->max_power;
 }
 
 static void handle_band(struct wiphy *wiphy,
@@ -1277,6 +1286,8 @@ static int ignore_request(struct wiphy *wiphy,
 	case NL80211_REGDOM_SET_BY_CORE:
 		return 0;
 	case NL80211_REGDOM_SET_BY_COUNTRY_IE:
+		if (wiphy->country_ie_pref & NL80211_COUNTRY_IE_IGNORE_CORE)
+			return -EALREADY;
 
 		last_wiphy = wiphy_idx_to_wiphy(last_request->wiphy_idx);
 
